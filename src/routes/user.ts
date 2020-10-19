df8e7f04277cc3db5edadfbc2a1e99772a8bce17
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-10-19 15:30:57 +0900

    Add documentations for user authentication

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,6 @@
 /**
  * @swagger
- *  tags:
+ * tags:
  *      name: User
  *      description: User와 관련한 라우트
  * definitions:
@@ -16,6 +16,28 @@
  *              password:
  *                  type: string
  *              nickname:
+ *                  type: string
+ *      user_auth_request:
+ *          type: object
+ *          required:
+ *              - email
+ *              - password
+ *          properties:
+ *              email:
+ *                  type: string
+ *              password:
+ *                  type: string
+ *      user_auth_response:
+ *          type: object
+ *          properties:
+ *              user:
+ *                  type: object
+ *                  properties:
+ *                      email:
+ *                          type: string
+ *                      nickname:
+ *                          type: string
+ *              token:
  *                  type: string
  */
 
@@ -38,6 +60,25 @@
  *                  description: User created
  *              400:
  *                  description: ParameterError
+ * /user/auth/token:
+ *      post:
+ *          tags:
+ *              - user
+ *          description: 주어진 정보로 인증 후 토큰 생성
+ *          produces:
+ *              - applicaion/json
+ *          parameters:
+ *              - in: "body"
+ *                required: true
+ *                schema:
+ *                      $ref: "#/definitions/user_auth_request"
+ *          responses:
+ *              200:
+ *                  schema:
+ *                      $ref: "#/definitions/user_auth_response"
+ *              400:
+ *                  schema:
+ *                      $ref: "#/definitions/error_response"
  */
 
 import { Router } from 'express'
@@ -53,8 +94,13 @@ const registerValidator = [
     body('nickname').notEmpty(),
     validationResultChecker
 ]
+const authValidator = [
+    body('email').notEmpty(),
+    body('password').notEmpty(),
+    validationResultChecker
+]
 
 router.post('/register', registerValidator, register)
-router.post('/auth/token', createToken)
+router.post('/auth/token', authValidator, createToken)
 
 export default router
